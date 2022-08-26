f6a3378c1d8807e6214f4405c6b157c93c8aa94b
SEVERIN Didier <65120153+dseverin2@users.noreply.github.com>
2022-08-26 18:52:24 +0400

    Update ubuntu-et-variantes-postinstall.sh

--- apps/ubuntu-et-variantes-postinstall.sh
+++ apps/ubuntu-et-variantes-postinstall.sh
@@ -283,12 +283,13 @@ if [ "$version" = "bionic" ] || [ "$version" = "focal" ] || [ "$version" = "jamm
 	apt install --no-install-recommends marble -y
 	
 	writelog "---OpenMeca"
-	apt-get install libqt5help5 libqt5svg5 libqt5opengl5 libqt5widgets5 libqt5gui5 libqt5xml5 libqt5core5a libboost-all-dev libqwt-qt5-6 libqglviewer2-qt5 -fy
-	if [ ! -e ./openmeca-64b.deb ]; then
-		wget "$wgetparams" --no-check-certificate https://openmeca.site/site/dl/openmeca_2.x_amd64.deb
-		dpkg -i openmeca*amd64.deb ; apt install -fy
-	fi
+	apt-get install libqt5help5 libqt5svg5 libqt5opengl5 libqt5widgets5 libqt5gui5 libqt5xml5 libqt5core5a libboost-all-dev libqwt-qt5-6 libqglviewer2-qt5 qttools5-dev libqwt-qt5-dev
+	wget "$wgetparams" --no-check-certificate https://openmeca.site/site/dl/openmeca_2.x_amd64.deb
+	sudo dpkg -i openmeca_2.x_amd64.deb
 	apt install --fix-broken -y
+	
+	writelog "---ScienceEngineering"
+	apt-get install -y science-engineering
 	
 	writelog "---BlueGriffon"
 	installfilename=bluegriffon-3.1.Ubuntu18.04-x86_64.deb
