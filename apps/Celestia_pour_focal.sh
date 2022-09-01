9eab3e5fe4a2d2a67b831f4ef20e318d19561107
SEVERIN Didier <65120153+dseverin2@users.noreply.github.com>
2022-09-02 00:53:05 +0400

    Update Celestia_pour_focal.sh

--- apps/Celestia_pour_focal.sh
+++ apps/Celestia_pour_focal.sh
@@ -6,10 +6,9 @@
 wget -nc -q http://fr.archive.ubuntu.com/ubuntu/pool/universe/g/glew/libglew2.1_2.1.0-4_amd64.deb
 apt install --no-install-recommends ./libglew2.1_2.1.0-4_amd64.deb -y
 rm -v libglew2.1_2.1.0-4_amd64.deb
-wget -nc -O- https://bintray.com/user/downloadSubjectPublicKey?username=bintray | apt-key add -
+wget -nc -O- https://celestia.space/packages/celestia.key | sudo apt-key add -
 
 # A remplacer par focal dès l'existence d'un fichier release :
-echo "deb https://dl.bintray.com/celestia/releases-deb bionic universe" | sudo tee /etc/apt/sources.list.d/celestia-obs.list
+echo "deb https://celestia.space/packages focal universe" | sudo tee /etc/apt/sources.list.d/celestia-obs.list
 apt update 
 apt install celestia -y
-rm -f /etc/apt/sources.list.d/celestia-obs.list
