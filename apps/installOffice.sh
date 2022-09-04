dac3ca13b0f34d2fae4effa0804b58a1d38d3545
SEVERIN Didier <65120153+dseverin2@users.noreply.github.com>
2022-09-04 14:47:13 +0400

    Update installOffice.sh

--- apps/installOffice.sh
+++ apps/installOffice.sh
@@ -6,6 +6,34 @@ wget -nc http://download.tuxfamily.org/polyglotte/archives/msfonts-config2.zip -
 unzip -o ./tmp_dl/msfonts-config2.zip -d /etc/fonts/
 apt-get install gsfonts gsfonts-other gsfonts-x11 ttf-mscorefonts-installer t1-xfree86-nonfree fonts-alee ttf-ancient-fonts fonts-arabeyes fonts-arphic-bkai00mp fonts-arphic-bsmi00lp fonts-arphic-gbsn00lp fonts-arphic-gkai00mp fonts-atarismall fonts-dustin fonts-f500 fonts-sil-gentium ttf-georgewilliams ttf-isabella fonts-larabie-deco fonts-larabie-straight fonts-larabie-uncommon ttf-sjfonts ttf-staypuft ttf-summersby fonts-ubuntu-title ttf-xfree86-nonfree xfonts-intl-european xfonts-jmk xfonts-terminus fonts-liberation ubuntu-restricted-extras -y
 
+#INSTALLATION DE WPS Office
+if $WPSOffice; then
+	writelog "------WPS Office + French"
+	unzip -o WPS\ Office/winfonts.zip -d /usr/share/fonts/winfonts/
+	rm -fr msfonts-config2.zip
+	build="9719"
+	version="11.1.0."$build
+
+	# Logiciel
+	wget http://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/$build/wps-office_$version.XA_amd64.deb
+	dpkg -i wps-office_$version.XA_amd64.deb
+	# Dictionnaire FR
+	apt install p7zip p7zip-full hunspell-fr -y
+	wget http://grammalecte.net/download/fr/hunspell-french-dictionaries-v6.4.1.zip
+	unzip hunspell*.zip	-d ./hunspell/
+	mkdir /opt/kingsoft/wps-office/office6/dicts/spellcheck/fr_FR/
+	printf "[Dictionary]\nDisplayName=Français\nDisplayName[zh_CN]=Français\nDisplayName[en_US]=Français" | tee /opt/kingsoft/wps-office/office6/dicts/spellcheck/fr_FR/dict.conf
+	printf "DisplayName[zh_TW]=Français\nDisplayName[zh_HK]=Français\nDisplayName[zh_MO]=Français\nDisplayName[zh_Hant_CN]=Français\n" | tee -a /opt/kingsoft/wps-office/office6/dicts/spellcheck/fr_FR/dict.conf
+	mv -fr ./hunspell/toutesvariantes.aff /opt/kingsoft/wps-office/office6/dicts/spellcheck/fr_FR/main.aff
+	# Interface FR
+	wget -c https://www.linuxtricks.fr/upload/wps-fr.tar.xz
+	tar xvf wps-fr.tar.xz
+	tar xvf wps-fr.tar -C /opt/kingsoft/wps-office/office6/mui/
+	cp -fr /opt/kingsoft/wps-office/office6/mui/fr_FR/* /opt/kingsoft/wps-office/office6/mui/ug_CN/
+	cp -fr /opt/kingsoft/wps-office/office6/mui/fr_FR/* /opt/kingsoft/wps-office/office6/mui/en_US/
+	rm -f wps-office_$version.XA_amd64.deb wps-fr* hunspell*
+fi
+
 # Extension CMathsOOo
 CMinstallfile="CmathOOo.oxt"	
 if [ ! -e $CMinstallfile ]; then
