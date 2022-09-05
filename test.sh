a0e87b9ad22cacddaa26fe774e9ba4cec3417235
SEVERIN Didier <65120153+dseverin2@users.noreply.github.com>
2022-09-05 13:27:53 +0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -203,12 +203,21 @@ apt install -y ldap-auth-client libpam-mount cifs-utils nscd numlockx unattended
 ########################################################################
 writelog "10a/42-Activation automatique des mises à jour de sécurité"
 echo "APT::Periodic::Update-Package-Lists \"1\";
+APT::Periodic::Download-Upgradeable-Packages \"1\";
+APT::Periodic::AutocleanInterval \"7\";
 APT::Periodic::Unattended-Upgrade \"1\";" > /etc/apt/apt.conf.d/20auto-upgrades 2>> $logfile
 
 ########################################################################
 # activation auto des mises à jour des paquets
 ########################################################################
 writelog "10b/42-Activation automatique des mises à jour des paquets"
+echo "Unattended-Upgrade::Allowed-Origins {
+        \"${distro_id}:${distro_codename}\";
+        \"${distro_id}:${distro_codename}-security\";
+        \"${distro_id}:${distro_codename}-updates\";
+//      \"${distro_id}:${distro_codename}-proposed\";
+//      \"${distro_id}:${distro_codename}-backports\";
+};" >> /etc/apt/apt.conf.d/50unattended-upgrades 2>> $logfile
 
 ########################################################################
 # Configuration du fichier pour le LDAP /etc/ldap.conf
@@ -377,22 +386,22 @@ if ! grep "/media/Serveur_Scribe" /etc/security/pam_mount.conf.xml  >/dev/null; 
 fi
 
 homes="<volume user=\"*\" fstype=\"cifs\" server=\"$scribe_def_ip\" path=\"perso\" mountpoint=\"~/Documents\" />"
-if ! grep "mountpoint=\"~\Documents"" /etc/security/pam_mount.conf.xml  >/dev/null; then
+if ! grep "mountpoint=\"~/Documents\"" /etc/security/pam_mount.conf.xml  >/dev/null; then
  sed -i "/<\!-- Volume definitions -->/a\ $homes" /etc/security/pam_mount.conf.xml 2>> $logfile
 fi
 
 groupes="<volume user=\"*\" fstype=\"cifs\" server=\"$scribe_def_ip\" path=\"groupes\" mountpoint=\"~/Groupes\" />"
-if ! grep "mountpoint=\"~\Groupes"" /etc/security/pam_mount.conf.xml  >/dev/null; then
+if ! grep "mountpoint=\"~/Groupes\"" /etc/security/pam_mount.conf.xml  >/dev/null; then
   sed -i "/<\!-- Volume definitions -->/a\ $groupes" /etc/security/pam_mount.conf.xml 2>> $logfile
 fi
 
 commun="<volume user=\"*\" fstype=\"cifs\" server=\"$scribe_def_ip\" path=\"commun\" mountpoint=\"~/Commun\" />"
-if ! grep "mountpoint=\"~\Commun"" /etc/security/pam_mount.conf.xml  >/dev/null; then
+if ! grep "mountpoint=\"~/Commun\"" /etc/security/pam_mount.conf.xml  >/dev/null; then
   sed -i "/<\!-- Volume definitions -->/a\ $commun" /etc/security/pam_mount.conf.xml 2>> $logfile
 fi
 
 professeurs="<volume user=\"*\" fstype=\"cifs\" server=\"$scribe_def_ip\" path=\"professeurs\" mountpoint=\"~/professeurs\" />"
-if ! grep "mountpoint=\"~\professeurs"" /etc/security/pam_mount.conf.xml  >/dev/null; then
+if ! grep "mountpoint=\"~/professeurs\"" /etc/security/pam_mount.conf.xml  >/dev/null; then
   sed -i "/<\!-- Volume definitions -->/a\ $professeurs" /etc/security/pam_mount.conf.xml 2>> $logfile
 fi
 
