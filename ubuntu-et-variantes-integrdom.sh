3fca6d170e86df867ca7330c9020ae9292ddaeaf
SEVERIN Didier <65120153+dseverin2@users.noreply.github.com>
2022-09-05 09:47:10 +0400

    Update ubuntu-et-variantes-integrdom.sh

--- ubuntu-et-variantes-integrdom.sh
+++ ubuntu-et-variantes-integrdom.sh
@@ -260,10 +260,10 @@ Default: yes
 Priority: 128
 Session-Type: Additional
 Session:
-       optional                        pam_mkhomedir.so silent" > /usr/share/pam-configs/mkhomedir 2>> $logfile
+       optional                        pam_mkhomedir.so silent" > /usr/share/pam-configs/mkhomedir
 
-addtoend /etc/pam.d/common-auth "auth    required     pam_group.so use_first_pass" 2>> $logfile
-addtoend /etc/pam.d/common-auth "auth  optional  pam_faildelay.so  delay=1000000"
+addtoend /etc/pam.d/common-auth "auth    required     pam_group.so use_first_pass"
+#addtoend /etc/pam.d/common-auth "auth  optional  pam_faildelay.so  delay=1000000"
 
 
 ########################################################################
