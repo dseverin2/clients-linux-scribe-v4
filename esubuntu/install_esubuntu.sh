bfb5a791abf4fbc9f69bf65165ebbbade5a38371
SEVERIN Didier <65120153+dseverin2@users.noreply.github.com>
2022-09-06 22:23:53 +0400

    Update install_esubuntu.sh

--- esubuntu/install_esubuntu.sh
+++ esubuntu/install_esubuntu.sh
@@ -81,7 +81,7 @@ writelog "ENDBLOC"
 ### Auto paramétrage de gset, firefox et conky
 ##############################################################################
 sudo -u "$SUDO_USER" find ./dans_icones/ -type f -exec sed -i -e "s/RNE_ETAB/"$rne_etab"/g" -e "s/IP_SCRIBE/"$scribe_def_ip"/g" -e "s/IP_PRONOTE/"$pronote"/g" -e "s/PORTAIL/"$portail"/g" -e "s/SALLEESU/"$salle"/g" {}\;
-sudo -u "$SUDO_USER" find ./dans_icones/ -type f -exec sed -i -e "s/GSETPROXYPORT/"$gset_proxy_port"/g" -e "s/GSETPROXY/"$gset_proxy"/g" -e "s/SUBNET/"$subnet"/g" {}\;
+sudo -u "$SUDO_USER" find ./dans_icones/ -type f -exec sed -i -e "s/PROXY_IP/"$proxy_def_ip"/g" -e "s/PROXY_PORT/"$proxy_def_port"/g" -e "s/GSETPROXYPORT/"$gset_proxy_port"/g" -e "s/GSETPROXY/"$gset_proxy"/g" -e "s/SUBNET/"$subnet"/g" {}\;
 interfaceeth=`ip -br link | grep 'UP' | grep -v 'OWN' | awk '{ print $1 }'`
 sudo -u "$SUDO_USER" find ./dans_icones/ -type f -exec sed -i -e "s/INTERFACEETH/"$interfaceeth"/g" {} \;
 
