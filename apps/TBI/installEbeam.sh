b65a48069c782cacd898d1ed06c6c595b5ac4b72
SEVERIN Didier <65120153+dseverin2@users.noreply.github.com>
2022-09-07 00:35:21 +0400

    Update installEbeam.sh

--- apps/TBI/installEbeam.sh
+++ apps/TBI/installEbeam.sh
@@ -4,12 +4,12 @@ echo "EBEAM : installation des dépendances"
 apt-get install libusb-dev yasm libvpx. Libx264. kazam onboard libboost-all-dev libboost-thread-dev -y
 
 echo "EBEAM : téléchargement et extraction de l\'archive"
-wget -nc http://down.myequil.com/dn/setup/Scrapbook_linux/eBeam-Interactive_3.6.tar.gz
-tar xvf eBeam-Interactive_3.6.tar.gz
-rm -fr eBeam-Interactive_3.6.tar.gz README.txt
+wget -nc https://speechi-support.speechi.net/EBEAM/eBeam-Interactive_3.6_ubuntu.tar.gz
+tar xvf eBeam-Interactive_3.6_ubuntu.tar.gz
+rm -fr eBeam-Interactive_3.6_ubuntu.tar.gz README.txt
 
 echo "EBEAM : installation du paquet eBeam-Interact_3"
-dpkg -i eBeam-Interact_3.6-0.deb
-rm -fr eBeam-Interact_3.6-0.deb
+dpkg -i eBeam-Interact_3.6.0.deb
+rm -fr eBeam-Interact_3.6.0.deb
 
 echo "EBEAM : Fin de l'installation"
