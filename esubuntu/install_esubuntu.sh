795c87ed96bfa2cd3830983ded603ddd086a7605
SEVERIN Didier <65120153+dseverin2@users.noreply.github.com>
2022-09-06 18:08:01 +0400

    Update install_esubuntu.sh

--- esubuntu/install_esubuntu.sh
+++ esubuntu/install_esubuntu.sh
@@ -80,6 +80,15 @@ writelog "ENDBLOC"
 ##############################################################################
 ### Auto paramétrage de gset, firefox et conky
 ##############################################################################
+##############################################################################
+### Auto paramétrage de gset, firefox et conky
+##############################################################################
+writelog "3/3-Autoparamétrage... OK"
+sudo -u "$SUDO_USER" find ./dans_icones/ -type f -exec sed -i -e "s/RNE_ETAB/"$rne_etab"/g" -e "s/IP_SCRIBE/"$scribe_def_ip"/g" -e "s/IP_PRONOTE/"$pronote"/g" -e "s/PORTAIL/"$portail"/g" -e "s/SALLEESU/"$salle"/g" {}\;
+sudo -u "$SUDO_USER" find ./dans_icones/ -type f -exec sed -i -e "s/GSETPROXYPORT/"$gset_proxy_port"/g" -e "s/GSETPROXY/"$gset_proxy"/g" -e "s/SUBNET/"$subnet"/g" {}\;
+interfaceeth=`ip -br link | grep 'UP' | grep -v 'OWN' | awk '{ print $1 }'`
+sudo -u "$SUDO_USER" find ./dans_icones/ -type f -exec sed -i -e "s/INTERFACEETH/"$interfaceeth"/g" {} \;
+
 if grep "posteslinux" "$chemin"/icones/gm_esu/linux/firefox.js > /dev/null; then
 	sed -i "s/posteslinux/$gm_esu/g" "$chemin"/icones/gm_esu/linux/firefox.js >> $logfile
 fi
