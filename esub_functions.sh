d13be82712c695c888e11cc42038562a7251dc23
SEVERIN Didier <65120153+dseverin2@users.noreply.github.com>
2022-09-11 01:12:21 +0400

    Update esub_functions.sh

--- esub_functions.sh
+++ esub_functions.sh
@@ -138,4 +138,5 @@ function addtoend {
 
 function autoparam {
   find $1/dans_icones/ -type f -exec sed -i -e "'s/RNE_ETAB/"$rne_etab"/g'" -e "'s/IP_SCRIBE/"$scribe_def_ip"/g'" -e "'s/IP_PRONOTE/"$pronote"/g'" -e "'s/PORTAIL/"$portail"/g'" -e "'s/SALLEESU/"$salle"/g'" -e "'s/PROXY_IP/"$proxy_def_ip"/g'" -e "'s/PROXY_PORT/"$proxy_def_port"/g'" -e "'s/GSETPROXYPORT/"$gset_proxy_port"/g'" -e "'s/GSETPROXY/"$gset_proxy"/g'" -e "'s/SUBNET/"$subnet"/g'" -e "'s/INTERFACEETH/"$interfaceeth"/g'" {} \; 2>> $logfile
+  echo find $1/dans_icones/ -type f -exec sed -i -e "'s/RNE_ETAB/"$rne_etab"/g'" -e "'s/IP_SCRIBE/"$scribe_def_ip"/g'" -e "'s/IP_PRONOTE/"$pronote"/g'" -e "'s/PORTAIL/"$portail"/g'" -e "'s/SALLEESU/"$salle"/g'" -e "'s/PROXY_IP/"$proxy_def_ip"/g'" -e "'s/PROXY_PORT/"$proxy_def_port"/g'" -e "'s/GSETPROXYPORT/"$gset_proxy_port"/g'" -e "'s/GSETPROXY/"$gset_proxy"/g'" -e "'s/SUBNET/"$subnet"/g'" -e "'s/INTERFACEETH/"$interfaceeth"/g'" {} \;
 }
