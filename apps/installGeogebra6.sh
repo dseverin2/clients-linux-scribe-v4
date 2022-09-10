7cbff919681c0ed4ad408c24265150756f6f9833
SEVERIN Didier <65120153+dseverin2@users.noreply.github.com>
2022-09-10 22:12:27 +0400

    Update installGeogebra6.sh

--- apps/installGeogebra6.sh
+++ apps/installGeogebra6.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # Script original de Didier SEVERIN (13/05/20)
-wget -nc -qO - https://static.geogebra.org/linux/office@geogebra.org.gpg.key | sudo apt-key add -
-echo 'deb http://www.geogebra.net/linux/ stable main' | tee /etc/apt/sources.list.d/geogebra-classic.list 
+sudo apt-add-repository -u 'deb http://www.geogebra.net/linux/ stable main' 
 sudo apt-get update
-apt install geogebra-classic -y
+sudo apt install geogebra 
