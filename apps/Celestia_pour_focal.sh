ed728cf55507b9ccee7b84b7214749d30438f762
SEVERIN Didier <65120153+dseverin2@users.noreply.github.com>
2022-09-11 21:10:26 +0400

    Update Celestia_pour_focal.sh

--- apps/Celestia_pour_focal.sh
+++ apps/Celestia_pour_focal.sh
@@ -6,7 +6,7 @@
 wget -nc -q http://fr.archive.ubuntu.com/ubuntu/pool/universe/g/glew/libglew2.1_2.1.0-4_amd64.deb
 apt install --no-install-recommends ./libglew2.1_2.1.0-4_amd64.deb -y
 rm -v libglew2.1_2.1.0-4_amd64.deb
-wget -nc -O- https://celestia.space/packages/celestia.key | sudo apt-key add -
+wget -nc -O- -q https://celestia.space/packages/celestia.key | sudo apt-key add -
 
 echo deb https://celestia.space/packages focal main | sudo tee /etc/apt/sources.list.d/celestia.list
 apt update 
