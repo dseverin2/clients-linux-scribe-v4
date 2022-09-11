3cc2388c261166617f17aa1ff953d7b954be36eb
SEVERIN Didier <65120153+dseverin2@users.noreply.github.com>
2022-09-11 11:50:51 +0400

    Update install_proxy_auth.sh

--- esubuntu/install_proxy_auth.sh
+++ esubuntu/install_proxy_auth.sh
@@ -62,6 +62,7 @@ Proxy		$proxy
 NoProxy		$proxy_env_noproxy
 Listen		3129" > /etc/cntlm.conf
 
+#sudo -u "$SUDO_USER"  sed -i -e "s/RNE_ETAB/$rne_etab/g" -e "s/IP_SCRIBE/$scribe_def_ip/g" -e "s/IP_PRONOTE/$pronote/g" -e "s/PORTAIL/$portail/g" -e "s/SALLEESU/$salle/g" -e "s/PROXY_IP/$proxy_def_ip/g" -e "s/PROXY_PORT/$proxy_def_port/g" -e "s/GSETPROXYPORT/$gset_proxy_port/g" -e "s/GSETPROXY/$gset_proxy/g" -e "s/SUBNET/$subnet/g" -e "s/INTERFACEETH/$interfaceeth/g" {} \; 2>> $logfile $baserep/esubuntu/icones/gm_esu/linux -type f -exec 
 
 echo "penser a modifier les paramétre proxy dans firefox.js et dans gset par 127.0.0.1:$port_cntlm"
 writelog "penser a modifier les paramétre proxy dans firefox.js et dans gset par 127.0.0.1:$port_cntlm"
