c4b9c7aa88f7a5fb25783054cfaa29369f964303
SEVERIN Didier <65120153+dseverin2@users.noreply.github.com>
2022-09-11 17:41:11 +0400

    Update installGeogebra6.sh

--- apps/installGeogebra6.sh
+++ apps/installGeogebra6.sh
@@ -2,4 +2,4 @@
 # Script original de Didier SEVERIN (13/05/20)
 sudo apt-add-repository -u 'deb http://www.geogebra.net/linux/ stable main' 
 sudo apt-get update
-sudo apt install geogebra 
+sudo apt install geogebra -y
