af27e56169163a11c033a64e3b6ac794d944d60d
SEVERIN Didier <65120153+dseverin2@users.noreply.github.com>
2022-09-11 15:45:27 +0400

    Update install_esubuntu.sh

--- esubuntu/install_esubuntu.sh
+++ esubuntu/install_esubuntu.sh
@@ -77,7 +77,7 @@ writelog "ENDBLOC"
 ### Auto paramétrage de gset, firefox et conky
 ##############################################################################
 interfaceeth=`ip -br link | grep 'UP' | grep -v 'OWN' | awk '{ print $1 }'`
-sudo -u "$SUDO_USER" find $baserep/dans_icones/groupe_esu/ -type f -exec sed -i -e "s/RNE_ETAB/$rne_etab/g" -e "s/IP_SCRIBE/$scribe_def_ip/g" -e "s/IP_PRONOTE/$pronote/g" -e "s/PORTAIL/$portail/g" -e "s/SALLEESU/$salle/g" -e "s/PROXY_IP/$proxy_def_ip/g" -e "s/PROXY_PORT/$proxy_def_port/g" -e "s/GSETPROXYPORT/$gset_proxy_port/g" -e "s/GSETPROXY/$gset_proxy/g" -e "s/SUBNET/$subnet/g" -e "s/INTERFACEETH/$interfaceeth/g" {} \; 2>> $logfile
+sudo -u "$SUDO_USER" find $baserep/dans_icones/groupe_esu/ -type f -exec sed -i -e "s/RNE_ETAB/$rne_etab/g" -e "s/IP_SCRIBE/$scribe_def_ip/g" -e "s/IP_PRONOTE/$pronote/g" -e "s/PORTAIL/$portail/g" -e "s/SALLEESU/$salle/g" -e "s/PROXY_IP/$proxy_def_ip/g" -e "s/PROXY_PORT/$proxy_def_port/g" -e "s/GSETPROXYPORT/$gset_proxy_port/g" -e "s/GSETPROXY/$gset_proxy/g" -e "s/SUBNET/$subnet/g" {} \; 2>> $logfile
 sudo -u "$SUDO_USER" find $baserep/esubuntu/icones/gm_esu/ -type f -exec sed -i -e "s/RNE_ETAB/$rne_etab/g" -e "s/IP_SCRIBE/$scribe_def_ip/g" -e "s/IP_PRONOTE/$pronote/g" -e "s/PORTAIL/$portail/g" -e "s/SALLEESU/$salle/g" -e "s/PROXY_IP/$proxy_def_ip/g" -e "s/PROXY_PORT/$proxy_def_port/g" -e "s/GSETPROXYPORT/$gset_proxy_port/g" -e "s/GSETPROXY/$gset_proxy/g" -e "s/SUBNET/$subnet/g" -e "s/INTERFACEETH/$interfaceeth/g" {} \; 2>> $logfile
 writelog "3/3-Autoparamétrage... OK"
 ##############################################################################
