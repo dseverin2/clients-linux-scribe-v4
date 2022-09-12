6f25ac0faece68a1460261de052f283aef24a9cc
SEVERIN Didier <65120153+dseverin2@users.noreply.github.com>
2022-09-13 01:29:31 +0400

    Update ubuntu-et-variantes-integrdom.sh

--- ubuntu-et-variantes-integrdom.sh
+++ ubuntu-et-variantes-integrdom.sh
@@ -378,7 +378,7 @@ fi
 writelog "INITBLOC" "24/42-Paramétrage pour remplir pam_mount.conf" "---/media/Serveur_Scribe"
 
 # Obsolète car n'affiche pas le contenu de manière récursive ?!
-eclairng="<volume user=\"*\" fstype=\"cifs\" server=\"$scribe_def_ip\" path=\"eclairng\" mountpoint=\"/media/Serveur_Scribe\" />"
+eclairng="<volume user=\"*\" fstype=\"cifs\" server=\"$scribe_def_ip\" path=\"eclairng\" mountpoint=\"/media/Serveur_Scribe/\" />"
 if ! grep "/media/Serveur_Scribe" /etc/security/pam_mount.conf.xml  >/dev/null; then
   sed -i "/<\!-- Volume definitions -->/a\ $eclairng" /etc/security/pam_mount.conf.xml 2>> $logfile
 fi
