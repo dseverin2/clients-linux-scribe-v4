748db84dfa8a17ac592a9f8667ddc7cad61e9f10
SEVERIN Didier <65120153+dseverin2@users.noreply.github.com>
2022-09-14 23:06:27 +0400

    Update installChromium.sh

--- apps/installChromium.sh
+++ apps/installChromium.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "install chromium"
 
-add-apt-repository ppa:system76/pop
-apt install chromium chromium-sandbox chromium-l10n chromium-shell chromium-driver
+add-apt-repository ppa:system76/pop -y
+apt install chromium chromium-sandbox chromium-l10n chromium-shell chromium-driver -y
 
 if [ -e /usr/bin/chromium-browser ]; then
         sudo ln -s /usr/bin/chromium-browser /usr/bin/chromium
