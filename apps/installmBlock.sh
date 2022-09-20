d9007f8d20a70ef0f1cf36b2b1618f4e3bf15470
SEVERIN Didier <65120153+dseverin2@users.noreply.github.com>
2022-09-20 23:02:24 +0400

    Update installmBlock.sh

--- apps/installmBlock.sh
+++ apps/installmBlock.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Test Root
-if [ $(id -u) -ne 0 ]; then                       
-    echo "Vous devez être root pour lancer ce script" >&2
+if [ $(id -u) -ne 0 ]; then
+	echo "Vous devez être root pour lancer ce script" >&2
     exit 1
 fi
 
@@ -12,25 +12,16 @@ apt-get install -y arduino
 
 # Installation de mBlock 4.0.4 (après récupération du .deb)
 apt-get -y install libgconf-2-4 
-if [ ! -e ./mBlock_4.0.4_amd64.deb ]; then
-	wget -nc -q "$wgetparams" --no-check-certificate http://mblock.makeblock.com/mBlock4.0/mBlock_4.0.4_amd64.deb
-fi
-if [ ! -e ./mLink-1.2.0-amd64.deb ]; then
-	wget -nc -q "$wgetparams" --no-check-certificate https://dl.makeblock.com/mblock5/linux/mLink-1.2.0-amd64.deb
-fi
+wget -nc -q "$wgetparams" --no-check-certificate http://mblock.makeblock.com/mBlock4.0/mBlock_4.0.4_amd64.deb
+wget -nc -q "$wgetparams" --no-check-certificate https://dl.makeblock.com/mblock5/linux/mLink-1.2.0-amd64.deb
 dpkg -i ./mBlock_4.0.4_amd64.deb
 dpkg -i ./mLink-1.2.0-amd64.deb
 apt install -fy
-
-rm -f mBlock_4.0.4_amd64.deb mLink-1.2.0-amd64.deb
 
 # Installation des librairies manquantes
 unzip -o $baserep/apps/mBlock.zip -d /opt/makeblock/mBlock
 chmod 777 /opt/makeblock/mBlock/resources/web/tmp/project.sb2
 
 #Installation des librairies pour arduino
-wget -q https://github.com/Makeblock-official/Makeblock-Libraries/archive/master.zip -O /usr/share/arduino/lib/master.zip
+wget -nc -q "$wgetparams" --no-check-certificate https://github.com/Makeblock-official/Makeblock-Libraries/archive/master.zip -O /usr/share/arduino/lib/master.zip
 unzip -o /usr/share/arduino/lib/master.zip -d /usr/share/arduino/lib/
-
-
-
