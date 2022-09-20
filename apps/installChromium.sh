c3bdca6ab637c1e6ce41a889b2e58084ca23704f
SEVERIN Didier <65120153+dseverin2@users.noreply.github.com>
2022-09-20 13:19:38 +0400

    maj chromium via apt ok

--- apps/installChromium.sh
+++ apps/installChromium.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 echo "install chromium"
-export http_proxy=http://$scribeuserapt:$scribepass@$proxy_def_ip:$proxy_def_port
-export https_proxy=http://$scribeuserapt:$scribepass@$proxy_def_ip:$proxy_def_port
-export HTTP_PROXY=http://$scribeuserapt:$scribepass@$proxy_def_ip:$proxy_def_port
-export HTTPS_PROXY=http://$scribeuserapt:$scribepass@$proxy_def_ip:$proxy_def_port
 sudo apt remove chromium-beta -y
 sudo snap remove chromium
 
 sudo -E add-apt-repository ppa:saiarcot895/chromium-beta -y
-apt install chromium-browser --install-suggests -y
+sudo apt install chromium-browser --install-suggests -y
 
 if [ -e /usr/bin/chromium-browser ]; then
 	sudo ln -s /usr/bin/chromium-browser /usr/bin/chromium
@@ -18,4 +14,5 @@ elif [ -e /usr/bin/chromium ]; then
 	sudo ln -s /etc/chromium /etc/chromium-browser
 fi
 
-cp -f $baserep/dans_icones/scripts/chromium/master_preferences /etc/chromium-browser/master_preferences
+sudo cp -f $baserep/dans_icones/scripts/chromium/master_preferences /etc/chromium-browser/master_preferences
+sudo ln -s /etc/chromium-browser/master_preferences /etc/chromium/master_preferences
