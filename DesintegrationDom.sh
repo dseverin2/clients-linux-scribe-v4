dcdd489b989b7a93e1bf627c346827d99d92e228
SEVERIN Didier <65120153+dseverin2@users.noreply.github.com>
2022-10-24 20:27:47 +0400

    Update DesintegrationDom.sh

--- DesintegrationDom.sh
+++ DesintegrationDom.sh
@@ -35,7 +35,9 @@ echo "" > /etc/crontab #remplacement du contenu d'esubuntu par du contenu vide
 
 echo "" > /etc/apt/apt.conf.d/20proxy
 sed '/proxy/d' /etc/environment | sudo tee /etc/environment
+sed '/PROXY/d' /etc/environment | sudo tee /etc/environment
 sed '/proxy/d' /etc/wgetrc | sudo tee /etc/wgetrc
+sed '/PROXY/d' /etc/wgetrc | sudo tee /etc/wgetrc
 snap set system proxy.http=""
 snap set system proxy.https=""
 
